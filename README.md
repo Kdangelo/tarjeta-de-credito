5608ef551aeb78a5ef482754e3795c396899008c
Kdangelo <kdangelof@gmail.com>
2017-10-05 17:17:18 -0300

    solucion tarjeta de credito valida

---
+++ README.md
@@ -0,0 +1,6 @@
+###### preguntar por el numero de tarjeta
+###### crear array
+###### pasar los numeros a un array en orden inverso
+###### aplicar operacion a los numeros pares
+###### sunmar los gigitos y nuevos digitos
+###### salida
---
+++ app.js
@@ -0,0 +1,18 @@
+// preguntar por el número de tarjeta con un prompt para obtener los numeros de entrada
+var creditCardNumber = prompt("Por favor, ingresa un número de tarjeta válido");
+// hacer que el prompt sean solo numeros
+// obtener los numeros de entrada y con estos construir un array
+creditCardNumber = new Array ();
+// pasar los números a un array en orden inverso para poder aplicar el logaritmo de Luhn
+for (var i = 0; i < array.length; i++) {
+	var item =  array.pop();
+	array.splice(i,0,item);
+	return array
+}	
+// chequear indices pares
+function checkSum(num); {
+	var len = num.length
+	var par = len % 2
+	var sum = 0
+	
+}
